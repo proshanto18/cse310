1ffd980b65cc4fc539c678d6ec8e967d0805e5da
Khalid Shahriar Kowshik <khalidkowshikcse@gmail.com>
2024-11-08 22:42:52 +0600

    register user functionality

--- amarcse/amarcse/views.py
+++ amarcse/amarcse/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect,render
+from django.contrib.auth.models import User
 
 def BASE(request):
     return render(request,'base.html')
@@ -7,7 +9,20 @@ def INDEX(request):
     return render(request,'components/index.html')
 
 def REGISTER(request):
-    return render(request,'components/register.html')
+    if request.method=='POST':
+        uname= request.POST.get('username')
+        email= request.POST.get('email')
+        pw1= request.POST.get('password1')
+        pw2= request.POST.get('password1')
+       
+        if pw1!=pw2:
+            return HttpResponse("passwords do not match")
+        else:
+            my_user= User.objects.create_user(uname,email,pw1)
+            my_user.save()
+            return redirect('/')
+    else:
+      return render(request,'components/register.html')
 
 def LOGIN(request):
     return render(request,'components/login.html')
