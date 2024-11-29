3747043644bb05c7097772bd2accd898d2dc6ccf
Tabassum73 <tabassumbintealamgiranchal7@gmail.com>
2024-11-29 15:47:44 +0600

    'exercisemodel'

--- amarcse/lectures/models.py
+++ amarcse/lectures/models.py
@@ -8,5 +8,24 @@ class Lecture(models.Model):
     video = models.FileField(upload_to='lectures/videos/', null=True, blank=True)
     image = models.ImageField(upload_to='lectures/images/', null=True, blank=True)
 
-    def __str__(self):
+    def _str_(self):
         return self.lecture_name
+
+
+class Exercise(models.Model):
+    lecture = models.ForeignKey(
+        Lecture,
+        on_delete=models.CASCADE,
+        related_name='exercises'
+    )
+    question_file = models.FileField(
+        upload_to='exercises/questions/',
+        help_text="Upload a text file containing the question."
+    )
+    answer_file = models.FileField(
+        upload_to='exercises/answers/',
+        help_text="Upload a text file containing the answer code."
+    )
+
+    def _str_(self):
+        return f"Exercise for {self.lecture.name} "
